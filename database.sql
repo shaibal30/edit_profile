680875b60d13fed02e7b6ed1bf57917b37f346e1
Ayon Chatterjee <ayonchatterjee2014@gmail.com>
2018-05-26 14:05:36 +0530

    initial status

---
+++ database.sql
@@ -0,0 +1,17 @@
+-----DBMS-------
+
+    CREATE DATABASE edit_profile;
+    USE edit_profile;
+    SELECT DATABASE();
+    CREATE TABLE user_info(
+      id INT NOT NULL AUTO_INCREMENT,
+      name VARCHAR(255) NOT NULL,
+      email VARCHAR(255) NOT NULL,
+      password VARCHAR(255) NOT NULL,
+      PRIMARY KEY(id)
+      );
+    DESC edit_profile;
+    
+-----DBMS----------
+
+
