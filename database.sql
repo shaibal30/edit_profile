ca10f46b686adf79cd5771e25489a7571a7ce21e
Shaibal Choudhury <shaibal534@gmail.com>
2018-05-28 14:40:23 +0530

    table name changed

--- database.sql
+++ database.sql
@@ -3,14 +3,14 @@
     CREATE DATABASE edit_profile;
     USE edit_profile;
     SELECT DATABASE();
-    CREATE TABLE user_info(
+    CREATE TABLE users(
       id INT NOT NULL AUTO_INCREMENT,
       name VARCHAR(255) NOT NULL,
       email VARCHAR(255) NOT NULL,
       password VARCHAR(255) NOT NULL,
       PRIMARY KEY(id)
       );
-    DESC user_info;
+    DESC users;
 
     
 -----DBMS----------
